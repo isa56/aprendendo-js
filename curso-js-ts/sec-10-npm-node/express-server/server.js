65cbae41a44a29521ff9933116fcac13d229f991
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2022-08-26 17:36:33 -0300

    Feat: Adiciona pasta de arquivos estáticos.

--- curso-js-ts/sec-10-npm-node/express-server/server.js
+++ curso-js-ts/sec-10-npm-node/express-server/server.js
@@ -4,11 +4,14 @@ const path = require("path");
 const routes = require("./routes");
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.set('views', path.resolve(__dirname, 'src', 'views'));  // determina o caminho para as views
-app.set('view engine', 'ejs');  // determina o motor de renderização
+app.set("views", path.resolve(__dirname, "src", "views")); // determina o caminho para as views
+app.set("view engine", "ejs"); // determina o motor de renderização
 
-app.listen(3000, () => console.log("Server is running @ http://localhost:3000"));
+app.listen(3000, () =>
+  console.log("Server is running @ http://localhost:3000")
+);
